2cc3d1c43ac58e7a388761e5154098617a9679b0
Yotam Salmon <yotam.salmon@gmail.com>
2018-06-05 21:16:56 +0300

    Activity host now communicates with embedded H5P

--- activity.php
+++ activity.php
@@ -16,8 +16,23 @@ if (!isset($_GET["a"])) {
 <html>
 <head>
     <title>H5P activity host</title>
+
+    <script type="text/javascript">
+        window.addEventListener("message", function(e) {
+            if (e.data["type"] && e.data.type == "xAPI") {
+                xAPI(e.data.event);
+            }
+        }, false);
+
+        window.xAPI = function(event) {
+            console.log("Received xAPI event: ", event);
+        };
+    </script>
 </head>
-<body>
-    This is where the activity comes.
+<body
+    style="margin: 0;">
+
+    <!-- H5P activity iframe -->
+    <iframe width="100%" height="100%" frameBorder="0" src="/h5p/embed/<?php echo($_GET["a"]); ?>" />
 </body>
 </html>
