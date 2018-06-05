66c7898269ef0423a9775d5cde75d194ff96b5b9
Yotam Salmon <yotam.salmon@gmail.com>
2018-06-05 20:45:33 +0300

    Headers fixed. Now correctly displaying embedded resources

--- proxy.php
+++ proxy.php
@@ -16,8 +16,8 @@ $url = "http://h5p.org" . $path;
 // Sending our HTTP request
 $response = file_get_contents($url, false);
 
-foreach ($http_response_header as $key => $val) {
-    header($key . ": " . $val);
+foreach ($http_response_header as $header) {
+    header($header);
     header("Access-Control-Allow-Origin: *");
 }
 
