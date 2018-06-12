1769df8489dbdfcf437030d25dba2c55e9a89753
Yotam Salmon <yotam.salmon@gmail.com>
2018-06-12 17:48:56 +0300

    Parameterize the template page

--- template.php
+++ template.php
@@ -1,3 +1,46 @@
+<?php
+    /* 
+    This is a problem XML generator.
+    POST parameters:
+        h5p         - the h5p embed id (the number only).
+        width       - the width of the activity inside edX. 
+                      Default: 100%
+
+        height      - the height of the activity inside edX.
+                      Default: 350px
+
+        fail        - the threshold for score 0. Below this score (percentage) the task will be graded 0.
+                      Default: 40%
+
+        success     - the threshhold for score 1. Above this score the task will be graded 1.
+                      Default: 85%
+
+                      A score between fail and success will result the task being graded 0.5
+        
+    */
+
+$_POST = $_GET;
+
+if (!isset($_POST["h5p"])) {
+    header("HTTP/1.1 400 Bad Request.");
+    die("No h5p parameter provided.");
+}
+$h5p = $_POST["h5p"];
+
+$width = isset($_POST["width"]) ? $_POST["width"] : "100%";
+$height = isset($_POST["height"]) ? $_POST["height"] : "350px";
+$fail = isset($_POST["fail"]) ? $_POST["fail"] : "40";
+$success = isset($_POST["success"]) ? $_POST["success"] : "85";
+
+if (isset($_POST["grade"]) && $_POST["grade"] != "false") {
+    $decimal_grade = "True";
+}
+else {
+    $decimal_grade = "False";
+}
+
+header("Content-Type: text/plain")
+?>
 <problem>
     <customresponse cfn="check_function">
         <script type="loncapa/python">
@@ -15,13 +58,18 @@ def check_function(e, ans):
             max_score += res["score"]["max"]
             achieved += res["score"]["raw"]
 
-    grade = 0 if max_score == 0 else (float(achieved) / float(max_score))
-    if grade < 0.4:
-        return {"ok": False, "msg": "Score: " + str(round(grade * 100)) + "%"}
-    elif grade < 0.85:
-        return {"ok": "Partial", "msg": "Score: " + str(int(round(grade * 100))) + "%"}
+    grade = 0 if max_score == 0 else (float(achieved) * 100 / float(max_score))
+    if grade <= <?php echo $fail; ?>:
+        ok = False
+    elif grade < <?php echo $success; ?>:
+        ok = "Partial"
     else:
-        return {"ok": True, "msg": "Score: " + str(round(grade * 100)) + "%"}
+        ok = True
+
+    if <?php echo $decimal_grade;?>:
+        return {"ok": ok, "msg": "Score: " + str(round(grade)) + "%", "grade_decimal": round(grade) / 100.0}
+    else:
+        return {"ok": ok, "msg": "Score: " + str(round(grade)) + "%"}
 
 ]]>
         </script>
@@ -31,10 +79,9 @@ def check_function(e, ans):
             get_statefn="window.getState"
             set_statefn="window.setState"
             initial_state="false"
-            width="100%"
-            height="325"
-            html_file="http://localhost:8080/activity?a=251488"
-            title="This is the problem title"
+            width="<?php echo $width; ?>"
+            height="<?php echo $height; ?>"
+            html_file="http://localhost:8080/activity?a=<?php echo $h5p;?>"
             sop="false"
             />
     </customresponse>
