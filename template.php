7009f13f952c0f5e99c9fcac715a47bb72c62e2c
Yotam Salmon <yotam.salmon@gmail.com>
2018-07-28 18:02:20 +0300

    Change URL in template.php

--- template.php
+++ template.php
@@ -81,7 +81,7 @@ def check_function(e, ans):
             initial_state="false"
             width="<?php echo $width; ?>"
             height="<?php echo $height; ?>"
-            html_file="https://h5p-edx.appspot.com/activity?a=<?php echo $h5p;?>"
+            html_file="https://h5p-edx.appspot.com/h5p/embed/<?php echo $h5p;?>"
             sop="false"
             />
     </customresponse>
