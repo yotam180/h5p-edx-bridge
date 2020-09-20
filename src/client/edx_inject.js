f8601931e13e3421fa5d5c6f8161fda60d7099d0
Yotam Salmon <yotam.salmon@gmail.com>
2020-09-20 12:08:33 +0300

    Experiments with submitting microphone based activities (some work)

--- src/client/edx_inject.js
+++ src/client/edx_inject.js
@@ -33,3 +33,31 @@
     $(document).ready(init);
   }
 })();
+
+
+
+$(() => {
+  const callback = (mutationList, observer) => {
+    console.log(mutationList, observer);
+    for (let mut of mutationList) {
+      if (mut.target.className !== "jsinput") {
+        continue;
+      }
+
+      console.log("JS input");
+      let iframe = $(mut.target).find("iframe")[0];
+      
+      const TEXT = '<iframe allow="camera *; microphone *"';
+      if (iframe.outerHTML.indexOf(TEXT) !== 0) {
+        iframe.outerHTML = iframe.outerHTML.replace("<iframe", TEXT);
+        console.log("Refreshed iframe");
+      }
+      else {
+        console.log("iframe is good");
+      }
+    }
+  }
+
+  const observer = new MutationObserver(callback);
+  observer.observe($("body")[0], {childList: true, subtree: true});
+})
